45e4769eff534db94e3e5c1ec3155deca8029841
Cecil Zorg <moroswitie@users.noreply.github.com>
2026-02-20 17:39:17 +0100

    Fixed narrowing type not being recognized (#12)
    * Fixed narrowing type not being recognized
    
    * increased test coverage
    
    * phpstan fixes

--- tests/Unit/PHPStan/Support/TypeCandidateResolverTest.php
+++ tests/Unit/PHPStan/Support/TypeCandidateResolverTest.php
@@ -5,10 +5,14 @@ declare(strict_types=1);
 namespace SquidIT\Tests\PhpCodingStandards\Unit\PHPStan\Support;
 
 use ArrayObject;
+use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Reflection\ReflectionProviderStaticAccessor;
+use PHPStan\Testing\PHPStanTestCase;
 use PHPStan\Type\Constant\ConstantBooleanType;
 use PHPStan\Type\NullType;
 use PHPStan\Type\ObjectType;
 use PHPStan\Type\UnionType;
+use PHPUnit\Framework\MockObject\Stub;
 use PHPUnit\Framework\TestCase;
 use SquidIT\PhpCodingStandards\PHPStan\Support\DenyList;
 use SquidIT\PhpCodingStandards\PHPStan\Support\TypeCandidateResolver;
@@ -26,6 +30,7 @@ final class TypeCandidateResolverTest extends TestCase
     {
         parent::setUp();
 
+        $this->registerDefaultReflectionProvider();
         $this->typeCandidateResolver = new TypeCandidateResolver();
     }
 
@@ -150,5 +155,148 @@ final class TypeCandidateResolverTest extends TestCase
 
         self::assertArrayNotHasKey('domainChild', $interfaceBaseNameToTypeMap);
         self::assertSame('DomainRootInterface', $interfaceBaseNameToTypeMap['domainRoot'] ?? null);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testResolveInterfaceBaseNameMapSkipsBuiltinInterfacesSucceeds(): void
+    {
+        $interfaceBaseNameToTypeMap = $this->typeCandidateResolver->resolveInterfaceBaseNameToTypeMap(
+            new UnionType([
+                new ObjectType(\ArrayAccess::class),
+                new ObjectType(DomainRootInterface::class),
+            ]),
+        );
+
+        self::assertArrayNotHasKey('arrayAccess', $interfaceBaseNameToTypeMap);
+        self::assertSame('DomainRootInterface', $interfaceBaseNameToTypeMap['domainRoot'] ?? null);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testResolveInterfaceBaseNameMapSkipsDenyListedCandidateNameSucceeds(): void
+    {
+        $typeCandidateResolver = new TypeCandidateResolver(
+            denyList: new DenyList(candidateNameList: ['domainRoot']),
+        );
+
+        $interfaceBaseNameToTypeMap = $typeCandidateResolver->resolveInterfaceBaseNameToTypeMap(
+            new ObjectType(DomainRootInterface::class),
+        );
+
+        self::assertSame([], $interfaceBaseNameToTypeMap);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testResolveNativeFallbackExpandsUserlandHierarchySucceeds(): void
+    {
+        $this->registerNativeFallbackReflectionProvider();
+        $candidateNameList = $this->typeCandidateResolver->resolvePHPStanType(new ObjectType(CustomDomainDto::class));
+        sort($candidateNameList);
+
+        self::assertSame(
+            [
+                'customDomain',
+                'customDomainDto',
+                'domainBase',
+                'domainBaseEntity',
+                'domainChild',
+                'domainRoot',
+            ],
+            $candidateNameList,
+        );
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testResolveNativeFallbackSkipsInternalClassesSucceeds(): void
+    {
+        $this->registerNativeFallbackReflectionProvider();
+        $candidateNameList = $this->typeCandidateResolver->resolvePHPStanType(new ObjectType(ArrayObject::class));
+
+        self::assertSame([], $candidateNameList);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testResolveNativeFallbackSkipsInternalParentsAndInterfacesSucceeds(): void
+    {
+        $this->registerNativeFallbackReflectionProvider();
+        $candidateNameList = $this->typeCandidateResolver->resolvePHPStanType(new ObjectType(UserlandDomainException::class));
+
+        self::assertSame(['userlandDomainException'], $candidateNameList);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testResolveInterfaceBaseNameMapNativeFallbackSkipsUnknownClassesSucceeds(): void
+    {
+        $this->registerNativeFallbackReflectionProvider();
+        $interfaceBaseNameToTypeMap = $this->typeCandidateResolver->resolveInterfaceBaseNameToTypeMap(
+            new ObjectType('SquidIT\Tests\PhpCodingStandards\Unit\PHPStan\Support\Fixtures\TypeCandidateResolver\UnknownInterface'),
+        );
+
+        self::assertSame([], $interfaceBaseNameToTypeMap);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testResolveInterfaceBaseNameMapNativeFallbackSkipsConcreteTypesSucceeds(): void
+    {
+        $this->registerNativeFallbackReflectionProvider();
+        $interfaceBaseNameToTypeMap = $this->typeCandidateResolver->resolveInterfaceBaseNameToTypeMap(
+            new ObjectType(CustomDomainDto::class),
+        );
+
+        self::assertSame([], $interfaceBaseNameToTypeMap);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testResolveInterfaceBaseNameMapNativeFallbackSupportsGlobalInterfacesSucceeds(): void
+    {
+        $this->registerNativeFallbackReflectionProvider();
+        $this->defineGlobalTypeCandidateResolverInterface();
+
+        $interfaceBaseNameToTypeMap = $this->typeCandidateResolver->resolveInterfaceBaseNameToTypeMap(
+            new ObjectType('TypeCandidateResolverGlobalInterface'),
+        );
+
+        self::assertSame(
+            ['typeCandidateResolverGlobal' => 'TypeCandidateResolverGlobalInterface'],
+            $interfaceBaseNameToTypeMap,
+        );
+    }
+
+    private function registerDefaultReflectionProvider(): void
+    {
+        // @phpstan-ignore-next-line phpstanApi.method
+        ReflectionProviderStaticAccessor::registerInstance(PHPStanTestCase::createReflectionProvider());
+    }
+
+    private function registerNativeFallbackReflectionProvider(): void
+    {
+        /** @var ReflectionProvider&Stub $reflectionProvider */
+        $reflectionProvider = self::createStub(ReflectionProvider::class);
+        $reflectionProvider->method('hasClass')->willReturn(false);
+
+        // @phpstan-ignore-next-line phpstanApi.method
+        ReflectionProviderStaticAccessor::registerInstance($reflectionProvider);
+    }
+
+    private function defineGlobalTypeCandidateResolverInterface(): void
+    {
+        if (interface_exists('TypeCandidateResolverGlobalInterface') === false) {
+            eval('interface TypeCandidateResolverGlobalInterface {}');
+        }
     }
 }
---
+++ tests/Unit/PHPStan/Support/VoDtoClassifierTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SquidIT\Tests\PhpCodingStandards\Unit\PHPStan\Support;
+
+use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Testing\PHPStanTestCase;
+use SquidIT\PhpCodingStandards\PHPStan\Support\VoDtoClassifier;
+use SquidIT\Tests\PhpCodingStandards\Unit\PHPStan\Support\Fixtures\TypeCandidateResolver\CustomDomainDto;
+use Throwable;
+
+final class VoDtoClassifierTest extends PHPStanTestCase
+{
+    private VoDtoClassifier $voDtoClassifier;
+    private ReflectionProvider $reflectionProvider;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->voDtoClassifier    = new VoDtoClassifier();
+        $this->reflectionProvider = self::createReflectionProvider();
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function testIsVoDtoClassCachesClassificationByClassNameSucceeds(): void
+    {
+        $classReflection      = $this->reflectionProvider->getClass(CustomDomainDto::class);
+        $firstClassification  = $this->voDtoClassifier->isVoDtoClass($classReflection);
+        $secondClassification = $this->voDtoClassifier->isVoDtoClass($classReflection);
+
+        self::assertSame($firstClassification, $secondClassification);
+    }
+}
