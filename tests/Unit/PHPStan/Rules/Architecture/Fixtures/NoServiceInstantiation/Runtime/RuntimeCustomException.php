7f9a9e5bccd8c9945d43481653ba4a68601f866f
Cecil Zorg <devop@cecilzorg.nl>
2026-02-28 16:10:11 +0100

    relaxed no service instantiating by allowing custom exception classes to be created with new

---
+++ tests/Unit/PHPStan/Rules/Architecture/Fixtures/NoServiceInstantiation/Runtime/RuntimeCustomException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SquidIT\Tests\PhpCodingStandards\Unit\PHPStan\Rules\Architecture\Fixtures\NoServiceInstantiation\Runtime;
+
+use RuntimeException;
+
+final class RuntimeCustomException extends RuntimeException
+{
+}
